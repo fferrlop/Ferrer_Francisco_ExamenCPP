fb941e42be9735e3a4596484caab5e7509186bcb
fferrlop <fferrlop@myuax.com>
2023-12-05 10:51:59 +0100

    clase environment

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+class environment {
+private:
+    std::map<std::string, std::string> env;
+
+public:
+
+};
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
